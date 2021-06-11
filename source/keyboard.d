85c2a6d5aa23e84ad0194e04f6b90ea53bfde334
Grim Maple <grimmaple95@gmail.com>
2021-06-11 22:45:04 +0300

    Use scan codes to differentiate NumPad inputs

--- source/keyboard.d
+++ source/keyboard.d
@@ -10,12 +10,31 @@ enum KeyState
 
 class KeyHook
 {
+    private enum ScanCodes
+    {
+        NumLock = 0x45,
+        ScrollLock = 0x46,
+        NumPad7 = 0x47,
+        NumPad8 = 0x48,
+        NumPad9 = 0x49,
+        NumPadMinus = 0x4A,
+        NumPad4 = 0x4B,
+        NumPad5 = 0x4C,
+        NumPad6 = 0x4D,
+        NumPadPlus = 0x4E,
+        NumPad1 = 0x4F,
+        NumPad2 = 0x50,
+        NumPad3 = 0x51,
+        NumPad0 = 0x52,
+        NumPadPeriod = 0x53
+    }
+
     private this() nothrow
     {
         hHook = SetHook();
         assert(hHook !is null);
         initConversionTable();
-        initNonExtensionConversion();
+        initScanCodesTable();
     }
 
     private void initConversionTable() nothrow
@@ -23,19 +42,20 @@ class KeyHook
         extendedConversion[VK_RETURN] = 0x3B; // Hacks :(
     }
 
-    private void initNonExtensionConversion() nothrow
+    private void initScanCodesTable() nothrow
     {
-        nonExtendedConversion[VK_INSERT] = VK_NUMPAD0;
-        nonExtendedConversion[VK_END] = VK_NUMPAD1;
-        nonExtendedConversion[VK_HOME] = VK_NUMPAD7;
-        nonExtendedConversion[VK_PRIOR] = VK_NUMPAD9;
-        nonExtendedConversion[VK_NEXT] = VK_NUMPAD3;
-        nonExtendedConversion[VK_DELETE] = VK_DECIMAL;
-        nonExtendedConversion[VK_LEFT] = VK_NUMPAD4;
-        nonExtendedConversion[VK_RIGHT] = VK_NUMPAD6;
-        nonExtendedConversion[VK_UP] = VK_NUMPAD8;
-        nonExtendedConversion[VK_DOWN] = VK_NUMPAD2;
-        nonExtendedConversion[VK_CLEAR] = VK_NUMPAD5;
+        scanCodes[ScanCodes.NumLock] = VK_NUMLOCK;
+        scanCodes[ScanCodes.NumPad0] = VK_NUMPAD0;
+        scanCodes[ScanCodes.NumPad1] = VK_NUMPAD1;
+        scanCodes[ScanCodes.NumPad2] = VK_NUMPAD2;
+        scanCodes[ScanCodes.NumPad3] = VK_NUMPAD3;
+        scanCodes[ScanCodes.NumPad4] = VK_NUMPAD4;
+        scanCodes[ScanCodes.NumPad5] = VK_NUMPAD5;
+        scanCodes[ScanCodes.NumPad6] = VK_NUMPAD6;
+        scanCodes[ScanCodes.NumPad7] = VK_NUMPAD7;
+        scanCodes[ScanCodes.NumPad8] = VK_NUMPAD8;
+        scanCodes[ScanCodes.NumPad9] = VK_NUMPAD9;
+        scanCodes[ScanCodes.NumPadPeriod] = VK_DECIMAL;
     }
 
     private static bool instantiated;
@@ -72,13 +92,16 @@ class KeyHook
             int vkCode = kb.vkCode;
             if(kb.flags & LLKHF_EXTENDED)
             {
+                // Windows doesn't distinguish NumPad enter and "normal" enter. If a numpad enter is pressed,
+                // an "extended" KeyDown event is generated
                 if(vkCode in KeyHook.get().extendedConversion)
                     vkCode = KeyHook.get().extendedConversion[vkCode];
             }
-            else
+            else if(kb.scanCode in KeyHook.get().scanCodes)
             {
-                if(vkCode in KeyHook.get().nonExtendedConversion)
-                    vkCode = KeyHook.get().nonExtendedConversion[vkCode];
+                // Normally Virtual Keys (VKs) don't distinguish between numpad and "real" keys. This is a hack
+                // that uses scan codes to understand if the pressed key is a numpad or not
+                vkCode = KeyHook.get().scanCodes[kb.scanCode];
             }
             try
             {
@@ -94,6 +117,7 @@ class KeyHook
         return CallNextHookEx(hook, nCode, wParam, lParam);
     }
 
-    private DWORD[DWORD] nonExtendedConversion;
     private DWORD[DWORD] extendedConversion;
+
+    private DWORD[DWORD] scanCodes;
 }
